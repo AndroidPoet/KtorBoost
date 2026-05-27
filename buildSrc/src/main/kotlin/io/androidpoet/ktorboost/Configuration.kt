b5cd0c370888c35808cf39052f6e7292b32c22fb
Ranbir Singh <poetdroid2@gmail.com>
2026-05-27 12:55:16 +0530

    chore: adopt Dropdown-style repository scaffolding

---
+++ buildSrc/src/main/kotlin/io/androidpoet/ktorboost/Configuration.kt
@@ -0,0 +1,14 @@
+package io.androidpoet.ktorboost
+
+object Configuration {
+  const val compileSdk = 34
+  const val targetSdk = 33
+  const val minSdk = 21
+  const val majorVersion = 1
+  const val minorVersion = 1
+  const val patchVersion = 6
+  const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+  const val versionCode = 1
+  const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
+  const val artifactGroup = "io.github.androidpoet"
+}
